b203266ad5f4dc90c14c08071dfddcd200651c99
Vitaly Agapov <wesallenalex@gmail.com>
2024-04-09 16:23:25 +0300

    move test.py to root

--- test.py
+++ test.py
@@ -138,15 +138,41 @@ print('---- recv_packet ----')
 
 recv_packet()
 
-# while True:
-#     print('---- recv_packet ----')
+test_requests = [
+#    bytearray(b'\x06\x00\x03\x0D\x00\x00\x03'), # read EPROM
+   bytearray(b'\x04\x00\x08\x00\x03'), # get AFR
+   bytearray(b'\x04\x00\x08\x01\x03'), # get Vbat
+   bytearray(b'\x04\x00\x08\x02\x03'), # get intake air temp
+   bytearray(b'\x04\x00\x08\x03\x03'), # get coolant temp
+   bytearray(b'\x03\x00\x05\x03'), # erase error codes
+   bytearray(b'\x04\x00\x08\x04\x03'), # get CO pot
+   bytearray(b'\x04\x00\x08\x05\x03'), # get O2 sensor
+   bytearray(b'\x04\x00\x08\x07\x03'), # get ignition time
+   bytearray(b'\x03\x00\x07\x03'), # get error codes
+   bytearray(b'\x06\x00\x01\x01\x00\x3a\x03'), # get RPM
+   bytearray(b'\x06\x00\x01\x01\x00\x20\x03'), # get TPS
+   bytearray(b'\x06\x00\x01\x01\x00\x42\x03'), # get engine load
+   bytearray(b'\x06\x00\x01\x02\x00\x62\x03'), # get injection time
+   bytearray(b'\x06\x00\x01\x01\x00\x22\x03'), # get ac drive/switch
+   bytearray(b'\x06\x00\x01\x01\x00\x29\x03'), # get O2 status
+   bytearray(b'\x06\x00\x01\x01\x01\x90\x03'), # get fuel pump relay status
+   bytearray(b'\x06\x00\x01\x01\x01\xb0\x03'), # get adsorber valve status
+#    bytearray(b'\x04\x00\x04\x0e\x03'),
+#    bytearray(b'\x04\x00\x04\x1f\x03'),
+#    bytearray(b'\x04\x00\x04\x21\x03'),
+   bytearray(b'\x03\x00\x06\x03'), # end session
+]
 
-#     recv_packet()
 
-#     time.sleep(0.02)
+for request in test_requests:
+    print('---- send_packet ----')
 
-#     print('---- send_packet ----')
+    send_packet(request)
 
-#     send_packet(bytearray(b'\x03\x02\x09\x03'))
+    time.sleep(0.05)
 
-#     time.sleep(0.05)
+    print('---- recv_packet ----')
+
+    recv_packet()
+
+    time.sleep(0.02)
